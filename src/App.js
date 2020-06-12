743e9dddc5f3a59378c339f796fbdf5b71a42dab
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-12 09:37:53 +0200

    error messages done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ class App extends Component {
     email: '',
     pass: '',
     accept: false,
+
+    errors: {
+      username: false,
+      email: false,
+      pass: false,
+      accept: false,
+    }
+  }
+
+  messages = {
+    username_incorrect: 'Nazwa musi być dłuższa niż 10 znaków i nie może zawierać spacji',
+    email_incorrect: 'Brak @ w emailu',
+    password_incorect: 'Hasło musi mieć 8 znaków',
+    accept_incorrect: 'Nie potwierdzona zgoda'
+
   }
 
   handleChange = (e) => {
@@ -41,16 +56,24 @@ class App extends Component {
         <form onSubmit={this.handleSubmit} noValidate>
           <label htmlFor="user">Twoje imię:
             <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+            {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
           </label>
+
           <label htmlFor="user">Twój adres email:
             <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+            {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
           </label>
+
           <label htmlFor="password">Twóje password:
             <input type="password" id="password" name="pass" value={this.state.email} onChange={this.handleChange} />
+            {this.state.errors.pass && <span>{this.messages.password_incofalse}</span>}
           </label>
+
           <label htmlFor="accept">
-            <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} />Wyrażam zgodę wszelaką
-          </label>
+            <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange}
+            />Wyrażam zgodę wszelaką
+            </label>
+          {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
 
           <button>Zapisz się</button>
         </form>
